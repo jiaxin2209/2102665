1022513c75aace085333c2b05e7397b49fd95688
jiaxin2209 <91715894+jiaxin2209@users.noreply.github.com>
2023-11-15 17:55:00 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 # Connect the Apache container to the existing network
-docker run -d -p 8888:80 --name my-apache-php-app-2 -v
+docker run -d -p 8888:80 --name my-apache-php-app -v
 /var/jenkins_home/workspace/Test-Pipeline-2/src:/var/www/html --network
 jenkins_network php:7.2-apache
 # Sleep for a short time to allow Apache container to start
